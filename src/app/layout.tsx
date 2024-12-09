f83695e1b12e2a8cea7fa4354bcef0006bd10cd8
Ahmed Khaled <ahmed.khaledswaby@gmail.com>
2024-12-09 23:56:34 +0200

    #feat: project init

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import { Cairo } from "next/font/google";
+
+const cairo = Cairo({ subsets: ["latin"] });
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cairo.className}>{children}</body>
+    </html>
+  );
+}
